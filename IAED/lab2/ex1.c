571c4d919472d0b856c758affdf1894be7e32687
Tiburso <44954856+Tiburso@users.noreply.github.com>
2020-02-24 19:39:33 +0000

    Add files via upload

---
+++ IAED/lab2/ex1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int a,b,c;
+
+  printf("Escreva três numeros:");
+  scanf("%d%d%d", &a,&b,&c);
+  if (a > b && a > c) {
+    printf("%d\n", a);
+  }
+  else if (b > a && b > c) {
+    printf("%d\n", b);
+  }
+  else
+    printf("%d\n", c);
+  return 0;
+}
