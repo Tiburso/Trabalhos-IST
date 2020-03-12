634b723e796355a7046223c8daf44f5bc252fb43
Tiburso <manuelmariamtc@gmail.com>
2020-03-12 16:58:19 +0000

    update no flappy bird ps: continua a n abrir com func

---
+++ FlappyBird/teste.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800,600))
+
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+    quit()
